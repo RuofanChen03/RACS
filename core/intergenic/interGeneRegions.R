74f74e5dba90fc014b3db0c97caf82c3f693ae57
ruofan chen <ruofan.is.awesome@gmail.com>
2025-07-02 03:12:26 -0400

    Reformatting & removing comments

--- core/intergenic/interGeneRegions.R
+++ core/intergenic/interGeneRegions.R
@@ -94,87 +94,89 @@ lstscfld <- c()
 lstregion1 <- c()
 lstregion2 <- c()
 lstSize <- c()
-#nbrEntries <- 20
 
 SCFLD <- ""
-for (i in c(1:nbrEntries)) {
-    scaffold <- levels(sortedDATA$lscaffold)[sortedDATA$lscaffold][i]
-    begReg <- sortedDATA[i,]$lbregion
-    endReg  <- sortedDATA[i,]$leregion
-    cat(i, scaffold,begReg,endReg,'\n')
-    if (SCFLD == scaffold) { 	#same scaffold
-	#if ((lstscfld[length(lstscfld)]==scaffold)&(begReg<lstregion2[length(lstscfld)])) {
-#	 if (begReg>lstregion1[try(length(lstscfld))]) {
-#	if (begReg>endReg) {
-	if  (begReg > sortedDATA[i-1,]$leregion) {
-       beginRegion <- begReg
-       #dumpData(SCFLD,endRegion,beginRegion, lstscfld,lstregion1,lstregion2,lstSize, 1)
-       myScfld <- dumpData(SCFLD,endRegion,beginRegion, 1)
+
+for (i in 1:nbrEntries) {
+  scaffold <- levels(sortedDATA$lscaffold)[sortedDATA$lscaffold][i]
+  begReg <- sortedDATA[i, ]$lbregion
+  endReg <- sortedDATA[i, ]$leregion
+  
+  cat(i, scaffold, begReg, endReg, '\n')
+  
+  if (SCFLD == scaffold) {
+    if (begReg > sortedDATA[i - 1, ]$leregion) {
+      beginRegion <- begReg
+      myScfld <- dumpData(SCFLD, endRegion, beginRegion, 1)
+      
+      lstscfld <- c(lstscfld, myScfld[1])
+      lstSize <- c(lstSize, myScfld[4])
+      lstregion1 <- c(lstregion1, endRegion + 1)
+      lstregion2 <- c(lstregion2, beginRegion - 1)
+      
+      endRegion <- endReg
+    } else {
+      print(">>>>>>>>>>>>>>>> SUSPICIOUS OVERLYING REGIONs!!! <<<<<<<<<<<<<<<<")
+      cat(scaffold, begReg, endReg, '\n')
+      cat(
+        try(lstscfld[length(lstscfld) - 1]),
+        try(lstregion1[length(lstscfld) - 1]),
+        try(lstregion2[length(lstscfld) - 1]), '\n'
+      )
+    }
+  } else {
+    if (SCFLD != "") {
+      beginRegion <- begReg
+      scfCAP <- scfldCAP(SCFLD, refTable)
+      
+      if (length(scfCAP) > 0) {
+        if (endRegion < scfCAP) {
+          myScfld <- dumpData(SCFLD, endRegion, scfCAP + 1, 1)
+          lstscfld <- c(lstscfld, myScfld[1])
+          lstSize <- c(lstSize, myScfld[4])
+          lstregion1 <- c(lstregion1, endRegion + 1)
+          lstregion2 <- c(lstregion2, scfCAP)
+        } else {
+          print(">>>>>>>>>>>>>>>> SUSPICIOUS OVERLYING REGIONs!!! <<<<<<<<<<<<<<<<")
+          cat(scaffold, begReg, endReg, '\n')
+          cat(
+            try(lstscfld[length(lstscfld) - 1]),
+            try(lstregion1[length(lstscfld) - 1]),
+            try(lstregion2[length(lstscfld) - 1]), '\n'
+          )
+        }
+        
+        myScfld <- dumpData(scaffold, '0', beginRegion, 1)
         lstscfld <- c(lstscfld, myScfld[1])
         lstSize <- c(lstSize, myScfld[4])
-        lstregion1 <- c(lstregion1,endRegion+1)
-        lstregion2 <- c(lstregion2,beginRegion-1)
-       endRegion <- endReg
-	} else {	# will to skip this region...
-		print(">>>>>>>>>>>>>>>> SUSPICIOUS OVERLYING REGIONs!!!  <<<<<<<<<<<<<<<<")
-		cat(scaffold,begReg,endReg,'\n')
-		cat(try(lstscfld[length(lstscfld)-1]),try(lstregion1[length(lstscfld)-1]),try(lstregion2[length(lstscfld)-1]),'\n')
-		#stop
-		}
-    } else { 	# change scaffold
-      if (SCFLD != "") {
-         beginRegion <- begReg
-         #dumpData(SCFLD,endRegion,'xxx', lstscfld,lstregion1,lstregion2,lstSize, 1)
-         scfCAP <- scfldCAP(SCFLD,refTable)
-         #DBG output: print(paste("XXX >~>~>",endRegion,"/",endReg," - ",scfCAP))
-         # cautionary guard: delaing with the case of  scfCAP  being empty ""  --> Integer(0)!!!
-         #  in principle due to data inconsistencies, keep it for robustness of the code
-         if (length(scfCAP)>0) {
-		if (endRegion < scfCAP) {	# dealing with cases where the gene ends at the limit of the scaffold...
-                   myScfld <- dumpData(SCFLD,endRegion,scfCAP+1, 1)
-                   lstscfld <- c(lstscfld, myScfld[1])
-                   lstSize <- c(lstSize, myScfld[4])
-                   lstregion1 <- c(lstregion1,endRegion+1)
-                   lstregion2 <- c(lstregion2,scfCAP)
-                } else {
-			print(">>>>>>>>>>>>>>>> SUSPICIOUS OVERLYING REGIONs!!!  <<<<<<<<<<<<<<<<")
-			cat(scaffold,begReg,endReg,'\n')
-			cat(try(lstscfld[length(lstscfld)-1]),try(lstregion1[length(lstscfld)-1]),try(lstregion2[length(lstscfld)-1]),'\n')
-			#stop
-           	}
-                #dumpData(scaffold,'0',beginRegion, lstscfld,lstregion1,lstregion2,lstSize, 1)
-                myScfld <- dumpData(scaffold,'0',beginRegion, 1)
-                lstscfld <- c(lstscfld, myScfld[1])
-                lstSize <- c(lstSize, myScfld[4])
-                lstregion1 <- c(lstregion1,0+1)
-                lstregion2 <- c(lstregion2,beginRegion-1)
-                endRegion <- endReg
-	 } else {
-		# potential data inconsistency...
-		cat("Potential data inconsistency issue detected... please verify your data integrity...",'\n\n')
-        }
-      } else { 	# first scaffold ever ...
-              beginRegion <- begReg	#strsplit(region,'-')[[1]][1]
-              endRegion <- endReg	#strsplit(region,'-')[[1]][2]
-              #dumpData(scaffold,'0',beginRegion, lstscfld,lstregion1,lstregion2,lstSize, 1)
-              myScfld <- dumpData(scaffold,'0',beginRegion, 1)
-               lstscfld <- c(lstscfld, myScfld[1])
-               lstSize <- c(lstSize, myScfld[4])
-               lstregion1 <- c(lstregion1,0+1)
-               lstregion2 <- c(lstregion2,beginRegion-1)
-         }
-      SCFLD <- scaffold
+        lstregion1 <- c(lstregion1, 1)
+        lstregion2 <- c(lstregion2, beginRegion - 1)
+        endRegion <- endReg
+      } else {
+        cat("Potential data inconsistency issue detected... please verify your data integrity...", '\n\n')
+      }
+    } else {
+      beginRegion <- begReg
+      endRegion <- endReg
+      myScfld <- dumpData(scaffold, '0', beginRegion, 1)
+      lstscfld <- c(lstscfld, myScfld[1])
+      lstSize <- c(lstSize, myScfld[4])
+      lstregion1 <- c(lstregion1, 1)
+      lstregion2 <- c(lstregion2, beginRegion - 1)
     }
-	if (length(lstregion1) != length(lstregion2)) stop()
+    SCFLD <- scaffold
+  }
+
+  if (length(lstregion1) != length(lstregion2)) stop()
 }
-# take care of last case...
-#dumpData(scaffold,endRegion,'xxx', lstscfld,lstregion1,lstregion2,lstSize, 1)
- scfCAP <- scfldCAP(scaffold,refTable)
- myScfld <- dumpData(scaffold,endRegion,scfCAP+1, 1)
-  lstscfld <- c(lstscfld, myScfld[1])
-  lstSize <- c(lstSize, myScfld[4])
- lstregion1 <- c(lstregion1,endRegion+1)
- lstregion2 <- c(lstregion2,scfCAP)
+
+scfCAP <- scfldCAP(scaffold, refTable)
+myScfld <- dumpData(scaffold, endRegion, scfCAP + 1, 1)
+
+lstscfld <- c(lstscfld, myScfld[1])
+lstSize <- c(lstSize, myScfld[4])
+lstregion1 <- c(lstregion1, endRegion + 1)
+lstregion2 <- c(lstregion2, scfCAP)
 
 interGenes <- data.frame(lstscfld,lstregion1,lstregion2,lstSize)
 names(interGenes) <- c("scaffold",'beggining','end','size')
